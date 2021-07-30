e0ea97f245db530f35df8c9fbca8276ee5e187d3
tiagogomez <tiago.gomezg@ggail.com>
2021-07-30 12:51:02 -0500

    JSON Parsing and code refactor

--- CryptocurrencyPriceTracker/ViewController.swift
+++ CryptocurrencyPriceTracker/ViewController.swift
@@ -12,25 +12,40 @@ class ViewController: UIViewController {
     @IBOutlet weak var picker: UIPickerView!
     @IBOutlet weak var price: UILabel!
     
-    var crypto: [String] = []
-    var currency: [String] = []
+    var crypto: [String] = ["BTC", "ETH", "XRP", "BCH"]
+    var currency: [String] = ["USD", "EUR", "JPY", "CHF"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.picker.delegate = self
         self.picker.dataSource = self
-        crypto = ["BTC", "ETH", "XRP", "BCH"]
-        currency = ["USD", "EUR", "JPY", "CHF"]
+        guard let firstCrypto = crypto.first,
+              let firstCurrency = currency.first else { return }
+        getPrice(crypto: firstCrypto, currency: firstCurrency)
     }
     
     private func getPrice(crypto: String, currency: String) {
         guard let url = URL(string: "https://min-api.cryptocompare.com/data/price?fsym=\(crypto)&tsyms=\(currency)") else { return }
         URLSession.shared.dataTask(with: url) { (data, response, error) in
             guard let data = data else {
-                print("Failure")
+                print("API Call Failure")
                 return
             }
-            print("Connected", data)
+            guard let json = try? JSONSerialization.jsonObject(with: data, options: []) as? [String : Double] else {
+                print("Parsing Failure")
+                return
+            }
+            guard let price = json[currency] else {
+                print("No Data Available")
+                return
+            }
+            let formatter = NumberFormatter()
+            formatter.currencyCode = currency
+            formatter.numberStyle = .currency
+            let formattedPrice = formatter.string(from: NSNumber(value: price))
+            DispatchQueue.main.async {
+                self.price.text = formattedPrice
+            }
         }.resume()
     }
 }
